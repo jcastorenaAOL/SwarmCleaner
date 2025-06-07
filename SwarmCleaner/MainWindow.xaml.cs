d7617b1001263c7c4146ffcb6f49714fdb3924c8
jcastorenaAOL <jcastorena@aolmfg.com>
2025-06-07 00:05:19 -0700

    Refresh Swarm II

--- SwarmCleaner/MainWindow.xaml.cs
+++ SwarmCleaner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,68 @@ namespace SwarmCleaner;
 /// </summary>
 public partial class MainWindow : Window
 {
+
+    List<Process> TBApps = new List<Process>();
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void RefreshSwarm(object sender, RoutedEventArgs e)
+    {
+
+        if (SwarmIsOpen())
+        {
+            foreach (Process process in TBApps)
+            {
+                if (!process.HasExited)
+                {
+                    // Graceful close (if supported)
+                    process.CloseMainWindow();
+                    process.WaitForExit(2000); // wait 2 seconds
+
+                    // If it's still not closed, force kill
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+        }
+        Thread.Sleep(3000);
+        if (!SwarmIsOpen())
+        {
+
+
+            Process.Start(@"C:\Program Files\Turtle Beach Swarm II\Turtle Beach Swarm II.exe");
+            //"C:\Program Files\Turtle Beach Swarm II\Turtle Beach Swarm II.exe"
+        }
+
+    }
+
+    private bool SwarmIsOpen()
+    {
+        TBApps.Clear();
+        TBApps.AddRange(Process.GetProcessesByName("Turtle Beach Swarm II"));
+        TBApps.AddRange(Process.GetProcessesByName("Turtle Beach Device Service"));
+
+        return TBApps.Count >= 1;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
     }
 }
